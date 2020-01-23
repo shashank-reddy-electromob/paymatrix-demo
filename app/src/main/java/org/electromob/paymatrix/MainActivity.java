f196e024da708f90c4afb16e2766c4e7198813bc
shashank-reddy-electromob <pshashankreddy67@gmail.com>
2020-01-23 21:05:57 +0530

    add codebase

---
+++ app/src/main/java/org/electromob/paymatrix/MainActivity.java
@@ -0,0 +1,37 @@
+package org.electromob.paymatrix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myWebView = (WebView)findViewById(R.id.paymatrix);
+
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://paymatrix.in");
+        myWebView.setWebViewClient(new WebViewClient());
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+}
---
+++ app/src/main/java/org/electromob/paymatrix/splashscreen.java
@@ -0,0 +1,42 @@
+package org.electromob.paymatrix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splashscreen);
+
+
+        //getSupportActionBar().hide();
+        LogoLauncher logoLauncher = new LogoLauncher();
+        logoLauncher.start();
+
+    }
+//camel-casing int myScore;
+    //kebab-casing int my_score;
+    private class LogoLauncher extends Thread{
+        public void run(){
+            try{
+                sleep(1000);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+
+            Intent intent = new Intent(splashscreen.this, MainActivity.class);
+            startActivity(intent);
+            splashscreen.this.finish();
+        }
+    }
+
+}
